1587ed63809f794c73cf9860c2d661b240e5e9d8
Manish Pardhan <pardhanmanish7887@gmail.com>
2025-11-21 11:22:04 +0530

    minor fixes

--- src/utils/socket.jsx
+++ src/utils/socket.jsx
@@ -1,9 +1,19 @@
 import { io } from "socket.io-client";
 
 let socket;
+
+const getSocketURL = () => {
+  if (window.location.hostname === "localhost") {
+    return "http://localhost:5000";
+  } else {
+    return "https://safestream-backend.onrender.com"; 
+  }
+};
+
 export const connectSocket = (userId) => {
   if (!socket) {
-    socket = io("http://localhost:5000", { withCredentials: true });
+    const socketURL = getSocketURL();
+    socket = io(socketURL, { withCredentials: true });
 
     socket.on("connect", () => {
       console.log("socket connected", socket.id);
